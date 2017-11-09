8efee0017f7ffeffecfc58b0762a6b0e121dcc7b
Dominique Dresen <dominique.dresen@uni-koeln.de>
2017-11-09 10:44:42 +0100

    Added all files for fortran calculation of models.

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
+gfortran -fPIC -O3 -c quadpack_double.f90
 f2py -m reflectivity --opt=-O3 --f90flags='-fopenmp' -lgomp \
       -I. quadpack_double.o\
-      -c MathFunctions.f90 ReflModels.f90 ReflNanoparticle.f90
+      -c Math.f90 Models.f90 Nanoparticle_Models.f90
